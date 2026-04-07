e3e2be6dd9cc0e226a9aa6243113b8682bd3e6cc
daeun <kde031126@gmail.com>
2026-04-07 16:09:57 +0900

    BE Update

---
+++ app/models/parent_report.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime, String
+from datetime import datetime
+from app.models.base import Base
+from sqlalchemy.orm import relationship
+
+class ParentReport(Base):
+    __tablename__ = "parent_reports"
+
+    report_id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+
+    period_type = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+    ment = Column(Text, nullable=True)
+    
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="reports")
