837fbd0d1d3ae65d21ef4744c7811fd9bce4b964
daeun <kde031126@gmail.com>
2026-04-08 01:58:54 +0900

    gitignore 파일 수정

--- app/models/step.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.models.base import Base
-
-class Step(Base):
-    __tablename__ = "steps"
-
-    step_id = Column(Integer, primary_key=True, index=True)
-    step_name = Column(String(100), nullable=False)
-    step_order = Column(Integer, nullable=False)
